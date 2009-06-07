4ac5119f074680ef62563d21c018bbe073fafba5
Nikos Dimitrakopoulos <nospam@nikosd.com>
2009-06-08 02:12:44 +0300

    Added examples.

---
+++ HopSharp.ConsoleExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HopSharp.ConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                new Exceptioner().Throw();
+            }
+            catch(Exception ex)
+            {
+                ex.SendToHoptoad();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Exceptioner
+    {
+        /// <exception cref="SomeCustomException"><c>SomeCustomException</c>.</exception>
+        public void Throw()
+        {
+            throw new SomeCustomException();
+        }
+    }
+
+    internal class SomeCustomException : ApplicationException
+    {
+        public SomeCustomException() : base("And a custom exception (arrriiibaaaaaa!!!!)"){}
+    }
+}
---
+++ HopSharp.WebExample/PostPage.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace HopSharp.WebExample
+{
+    public partial class PostPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.RequestType == "POST")
+                throw new ApplicationException("An exception inside a POST action.");
+        }
+    }
+}
